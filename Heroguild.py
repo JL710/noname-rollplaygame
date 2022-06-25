a9aef7193688b140b470351b9323067eee1757ff
JL710 <jan.lyncker@freenet.de>
2022-06-25 15:11:43 +0200

    Create Heroguild.py

---
+++ Heroguild.py
@@ -0,0 +1,21 @@
+from Figure import Figure
+from typing import Literal
+
+class HeroGuild:
+    def __init__(self, name: str, heros: list):
+        self.__name = name
+        self.__heros = [Figure(hero) for hero in heros]
+        self.__xp = 0
+
+    def raising(self, attribute: Literal["fight", "skill", "crafting", "crafting", "intelligence", "weapon"]):
+        if self.__xp >= 100:
+            # TODO: do shit with the hero
+
+            self.__xp -= 100
+            return True
+        return False
+
+    def set_xp(self, amount: int):
+        """will add the amount to the guilds xp"""
+        self.__xp += amount
+
